6a9ce171851aeb143adda0ed82fa9e4bdd712e54
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2022-04-13 09:28:49 -0500

    Create simulate_step.py

---
+++ operational_rdbms/source/simulate_step.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+def simulate_step(data_for_ingestion_path, rdbms_path):
+
+    rdbms = pd.read_csv(rdbms_path)
+    last_date = rdbms.date.tail(1).values[0]
+
+    data = pd.read_csv(data_for_ingestion_path)
+    data = data[data.date > last_date]
+
+    init_index = data.index[0]
+    end_index = init_index
+    is_first_monday = True
+    for index in data.index:
+        if data.day_name[index] == "Monday":
+            if is_first_monday is True:
+                is_first_monday = False
+            else:
+                break
+        end_index = index
+
+    data_to_append = data.loc[init_index:end_index, :]
+
+    print(data_to_append)
+
+    rdbms = pd.concat([rdbms, data_to_append])
+    rdbms.to_csv(rdbms_path, index=False)
+
+
+def main():
+
+    simulate_step(
+        data_for_ingestion_path="data/rdbms/historical_letter_data.csv",
+        rdbms_path="data/rdbms/letter_rdbms.csv",
+    )
+
+    simulate_step(
+        data_for_ingestion_path="data/rdbms/historical_web_data.csv",
+        rdbms_path="data/rdbms/web_rdbms.csv",
+    )
+
+
+if __name__ == "__main__":
+    main()
